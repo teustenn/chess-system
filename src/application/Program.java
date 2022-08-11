9b0421a9cbfc5d6ad6e43142e5526d6896ea8857
teustenn <teustenn@gmail.com>
2022-08-11 15:23:27 -0300

    First Class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,20 @@
 package application;
 
+import java.util.Scanner;
+
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		Position pos = new Position(3, 5);
+		
+		System.out.print(pos);
+		
+		sc.close();
 
 	}
 
