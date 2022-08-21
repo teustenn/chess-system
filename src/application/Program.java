465fb06e568abea8417d4e73ae150848447bf504
teustenn <teustenn@gmail.com>
2022-08-21 15:13:16 -0300

    Special Move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,6 +37,12 @@ public class Program {
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);					
 				}
+				
+				if (chessMatch.getPromoted() != null) {
+					System.out.print("Piece for Promotion (B/H/R/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
 			}
 			catch (ChessException e){
 				System.out.println();
